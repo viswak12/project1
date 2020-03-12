4331d1834266f98d981b49869f2d7a29f68f4ae5
viswak12 <tiruveswak1222@gmail.com>
2020-03-12 15:56:24 +0530

    Updates

---
+++ precache-manifest.3f124e46cd44151dbbe915dfc917bf47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1b692cde21269cc5773dd9f6948dfc",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "da041834af57b1c0f4d3",
+    "url": "/project1/static/css/main.91740dfe.chunk.css"
+  },
+  {
+    "revision": "2fc8a45fa92345f9fd1d",
+    "url": "/project1/static/js/2.1e8fc4ff.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.1e8fc4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da041834af57b1c0f4d3",
+    "url": "/project1/static/js/main.125c5721.chunk.js"
+  },
+  {
+    "revision": "fe46e28af8f8da747adb",
+    "url": "/project1/static/js/runtime-main.25af23f5.js"
+  },
+  {
+    "revision": "c256d4cdfab928e48a0eedacf89be4a9",
+    "url": "/project1/static/media/pic2.c256d4cd.svg"
+  },
+  {
+    "revision": "821c57b8b5f3df39b42c58c6131fbb59",
+    "url": "/project1/static/media/pic5.821c57b8.svg"
+  }
+]);
